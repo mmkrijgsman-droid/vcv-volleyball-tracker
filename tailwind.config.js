de5d7495ccb998f77000087a56e3886010ab79c3
mmkrijgsman-droid <mm.krijgsman@gmail.com>
2026-02-07 10:16:15 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'vcv-red': '#dc2626',
+        'vcv-blue': '#2563eb',
+        'vcv-orange': '#fb923c',
+        'vcv-yellow': '#fbbf24',
+      },
+    },
+  },
+  plugins: [],
+}
